2aac9d6d2f81da20714e602e6712511834ed5db7
giaongo <giaongo.97@gmail.com>
2024-11-24 17:19:27 +0200

    complete big O

--- BigO/exercises.js
+++ BigO/exercises.js
@@ -1,14 +1,14 @@
 /* Exercise 1 ------------------*/
 function funChallenge(input) {
-    let a = 10; // O[1]
-    a = 50 + 3; // O[1]
+    let a = 10; // O(1)
+    a = 50 + 3; // O(1)
   
-    for (let i = 0; i < input.length; i++) { // O[n]
-      anotherFunction(); // O[n]
-      let stranger = true; // O[n]
-      a++; // O[n]
+    for (let i = 0; i < input.length; i++) { // O(n)
+      anotherFunction(); // O(n)
+      let stranger = true; // O(n)
+      a++; // O(n)
     }
-    return a; // O[1]
+    return a; // O(1)
   }
 
   // Big O(3 + 4n)
@@ -54,6 +54,37 @@ function printPair(boxes) {
     }
   }
 }
-printPair(boxes)
+// printPair(boxes)
 
 // Big O is O(n^2) -> Quadratic time
+
+/* Exercise 4 -------------------*/
+//#5 Space complexity O(1)
+function boooo(n) {
+  for (let i = 0; i < n; i++) {
+    console.log("booooo");
+  }
+}
+
+// boooo([1,2,3,4,5]) // space complexity: O(1)
+
+function arrayOfHiNTimes(n) {
+  var hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = "hi";
+  }
+  return hiArray;
+}
+
+// arrayOfHiNTimes(6); // space complexity: O[n]
+
+
+/* Exercise 5 -------------------*/
+// Find 1st, Find Nth
+const array = ["hi", "my", "teddy"]
+// array[0] // O(1)
+//barray[array.length-1] // O(1)
+
+array.forEach(value => {
+  console.log("value is ", value)
+})
